14ec3bb37f490c901917be5b4011072056cde248
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-03-14 10:12:16 +0300

    async handling

--- dev/Express/day22/book-library/backend/src/middlewares/asyncHandler.ts
+++ dev/Express/day22/book-library/backend/src/middlewares/asyncHandler.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-
+/**
+ * @desc- avoid the problem of try catch not automatically passed to asynchronous
+ * @param fn The asynchronous function to wrap async functions
+ * @returns A function that executes the async function and catches the error
+ */
 const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
