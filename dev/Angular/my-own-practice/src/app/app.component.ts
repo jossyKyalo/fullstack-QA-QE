264a98b54832acdda8e536e28b3fa6c39ec8c347
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-03-27 16:02:41 +0300

    I love love Angular

--- dev/Angular/my-own-practice/src/app/app.component.ts
+++ dev/Angular/my-own-practice/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<h1>Hello World! I\'m learning Angular😍</h1>',
+  imports: [RouterOutlet, DataBindingComponent, NgIfComponent],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
