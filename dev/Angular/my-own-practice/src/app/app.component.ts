5e545adfedcbea487a5bebe6df1659ce80dc67de
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-04-06 23:54:23 +0300

    ng style

--- dev/Angular/my-own-practice/src/app/app.component.ts
+++ dev/Angular/my-own-practice/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from "./ng-class/ng-class.component";
+import { NgStyleComponent } from "./ng-style/ng-style.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent, NgIfComponent, NgForComponent, NgClassComponent],
+  imports: [RouterOutlet, DataBindingComponent, NgIfComponent, NgForComponent, NgClassComponent, NgStyleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
