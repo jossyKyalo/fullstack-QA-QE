c7c55ac812009b5bf2a3daf7ff2db4aa7f0a3388
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-04-07 10:43:15 +0300

    routing

--- dev/Angular/my-own-practice/src/app/app.component.ts
+++ dev/Angular/my-own-practice/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { NgIfComponent } from './ng-if/ng-if.component';
-import { NgForComponent } from './ng-for/ng-for.component';
-import { NgClassComponent } from "./ng-class/ng-class.component";
-import { NgStyleComponent } from "./ng-style/ng-style.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import { DataBindingComponent } from './data-binding/data-binding.component';
+// import { NgIfComponent } from './ng-if/ng-if.component';
+// import { NgForComponent } from './ng-for/ng-for.component';
+// import { NgClassComponent } from "./ng-class/ng-class.component";
+// import { NgStyleComponent } from "./ng-style/ng-style.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent, NgIfComponent, NgForComponent, NgClassComponent, NgStyleComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- dev/Angular/my-own-practice/src/app/app.routes.ts
+++ dev/Angular/my-own-practice/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { ControlFlowComponent } from './control-flow/control-flow.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //default route
+    {
+        path:'',
+        redirectTo:'dataBinding',
+        pathMatch:'full'
+    },
+    {
+        path:'dataBinding',
+        component: DataBindingComponent
+    },
+    {
+        path:'ng-class',
+        component:  NgClassComponent
+    },
+    {
+        path: 'ng-style',
+        component: NgStyleComponent
+    }
+];
