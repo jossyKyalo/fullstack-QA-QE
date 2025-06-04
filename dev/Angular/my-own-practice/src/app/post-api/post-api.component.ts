45866617ae7f217fe366f56e24dc1ce528bce5e7
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-06-05 01:32:22 +0300

    post api

--- dev/Angular/my-own-practice/src/app/post-api/post-api.component.ts
+++ dev/Angular/my-own-practice/src/app/post-api/post-api.component.ts
@@ -8,12 +8,35 @@ import { Component, inject } from '@angular/core';
   styleUrl: './post-api.component.css'
 })
 export class PostApiComponent {
-  carList: any[]=[];
-  http= inject(HttpClient);
-  getAllCars(){
-    this.http.get('https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars').subscribe((res: any)=>{
-       this.carList = res.data;
+  carList: any[] = [];
+  carObj: any = {
+    "carId": "",
+    "brand": "",
+    "model": "",
+    "year": "",
+    'color': "",
+    'dailyRate':"" ,
+    'carImage': "",
+    'regNo': ""
+  }
+  http = inject(HttpClient);
+  getAllCars() {
+    this.http.get('https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars').subscribe((res: any) => {
+      this.carList = res.data;
     })
+  }
+  onSaveCar() {
+    this.http.post('https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar', this.carObj).subscribe((res: any) => {
+      if (res.status == 200) {
+        alert("Car added successfully");
+        this.getAllCars();
+      } else {
+        alert("Error adding car");
+      }
+    }, (err) => {
+      console.error(err);
+      alert("An error occurred while adding the car");
+    });
   }
 
 }
