dbfb34abce36c614f4dff7dcd9c8547d8e69125f
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-06-18 22:44:30 +0300

    interceptors

---
+++ dev/Angular/my-own-practice/src/app/interceptor/token.interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token= localStorage.getItem('token');
+  const newReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }});      
+  return next(newReq);
+};
