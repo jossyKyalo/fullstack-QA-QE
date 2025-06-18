7abdf426cf9cffb2c68ac00bfe7735bb28061e97
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-06-18 23:35:21 +0300

    tokens, local storage ...

---
+++ dev/Angular/my-own-practice/src/app/interceptor/error.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { CustomerService } from '../services/customer.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const custService= inject(CustomerService);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        const isContinue= confirm("Are you sure you want to continue?");
+        if (isContinue) {
+           custService.tokenExpired$.next(true);
+        }  
+      }
+    return throwError(error)
+  }));
+};
